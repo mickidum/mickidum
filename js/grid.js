0af74ee44dfb7775c7a4e50402e2f1ed05f1dcc9
mickidum <mickidum@gmail.com>
2017-02-05 10:36:46 +0200

    Update grid.js

--- js/grid.js
+++ js/grid.js
@@ -352,7 +352,7 @@ var Grid = (function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
 			this.$description = $( '<p></p>' );
-			this.$href = $( '<a href="#">Show Live Preview</a>' );
+			this.$href = $( '<a rel="nofollow" href="#">Show Live Preview</a>' );
 			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
